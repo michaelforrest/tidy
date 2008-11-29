b622af20617a39d2d7d86f342cd7fc53538705e1
Michael Forrest <michael.forrest@gmail.com>
2008-11-29 12:39:33 +0000

    copy fixes

--- lib/g_project.rb
+++ lib/g_project.rb
@@ -12,9 +12,11 @@ class GProject
     folder("aslibs/#{LANGUAGE}/lbi", 'lib')
   end
   def folder(source,dest)
+    puts "adding library to this project's #{dest} folder"
     dest = "#{@project_dir}/#{dest}/"
     FileUtils.mkdir_p(dest)
     FileUtils.cp_r("#{File.dirname(File.expand_path(__FILE__))}/#{source}", dest)
+
   end
    
 end
