597e61f065c7df66dde91cd38ff2726ab1159157
Michael Forrest <michael.forrest@gmail.com>
2008-11-29 12:34:08 +0000

    hooray. it works. it's really really basic and stupid but.. well.. it's better than copying and pasting files by hand...

--- lib/g_project.rb
+++ lib/g_project.rb
@@ -1,12 +1,20 @@
+require 'fileutils'
 class GProject
-  ROOT = File.dirname(__FILE__) + '/..'
-  def initialize(args={})
+  LANGUAGE = 'as3'
+  def initialize(options={:dir=>"."})
+    @project_dir = options[:dir]
     puts 'Run this in your project folder after sprouts -n as3 SomeProject (will automate and make this all simpler soon)'
     puts "====================================================================="
     puts `pwd`
+    puts File.expand_path(__FILE__)
     puts "====================================================================="
-    File.copy("script/lbi", "script")
+    folder('script/lbi', 'script')
+    folder("aslibs/#{LANGUAGE}/lbi", 'lib')
   end
-  
-  
+  def folder(source,dest)
+    dest = "#{@project_dir}/#{dest}/"
+    FileUtils.mkdir_p(dest)
+    FileUtils.cp_r("#{File.dirname(File.expand_path(__FILE__))}/#{source}", dest)
+  end
+   
 end
--- test/test_g_project.rb
+++ test/test_g_project.rb
@@ -4,16 +4,17 @@ require 'ftools'
 class TestGProject < Test::Unit::TestCase
   PROJECT_FOLDER = 'tmp/TestProject'
   def setup
-    jump_into(PROJECT_FOLDER)
+   # jump_into(PROJECT_FOLDER)
+   
   end
   def test_add_libs
-    GProject.new
-    Dir.mkdir('script')
+    GProject.new(:dir=>PROJECT_FOLDER)
+    #Dir.mkdir('script')
     assert File.exists?('script/lbi')
   end
-  def jump_into(dir)
-    Dir.rmdir(dir) if File.exists?(dir)
-    Dir.mkdir(dir)
-    Dir.chdir(dir)
-  end
+  #def jump_into(dir)
+  #  #Dir.rmdir(dir) 
+  #  Dir.mkdir(dir) unless File.exists?(dir)
+  #  Dir.chdir(dir)
+  #end
 end
