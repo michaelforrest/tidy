6862a7d8ea12371ff83fb34df2cd03f6b7d5cbfb
Michael Forrest <michael.forrest@gmail.com>
2009-08-15 12:45:47 +0100

    got tests working again

--- test/test_tidy_project.rb
+++ test/test_tidy_project.rb
@@ -1,16 +1,18 @@
 require "test/unit"
-require 'lib/g_project'
+require 'lib/tidy_project'
 require 'ftools'
-class TestGProject < Test::Unit::TestCase
+class TestTidyProject < Test::Unit::TestCase
   PROJECT_FOLDER = 'tmp/TestProject'
   def setup
    # jump_into(PROJECT_FOLDER)
    
   end
   def test_add_libs
-    GProject.new(:dir=>PROJECT_FOLDER)
-    #Dir.mkdir('script')
-   # assert File.exists?('script/lbi')
+    TidyProject.new(PROJECT_FOLDER)
+    Dir.chdir PROJECT_FOLDER do
+      assert File.exists?('script'), "No script folder"
+      assert File.exists?('rakefile'), "No rakefile"
+    end
   end
   #def jump_into(dir)
   #  #Dir.rmdir(dir) 
